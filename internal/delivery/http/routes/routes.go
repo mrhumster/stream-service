c3827cbeabaf822b69b7b5c76289738f6dd4b5f0
Om Ra <me@xomrkob.ru>
2026-02-04 16:10:00 +0600

    change prefix api

--- internal/delivery/http/routes/routes.go
+++ internal/delivery/http/routes/routes.go
@@ -70,12 +70,12 @@ func SetupRoutes(db *gorm.DB, mode string, permissionClient auth.PermissionClien
 	auth.Use(middleware.AuthMiddleware(tokenService))
 	auth.Use(middleware.Authorize("stream", "read", permissionClient))
 	{
-		auth.POST("/api/content", middleware.Authorize("stream", "write", permissionClient), streamHandler.CreateStream)
-		auth.GET("/api/content/:id", middleware.Authorize("stream", "read", permissionClient), streamHandler.GetStream)
-		auth.PATCH("/api/content/:id", middleware.Authorize("stream", "write", permissionClient), streamHandler.UpdateStream)
-		auth.DELETE("/api/content/:id", middleware.Authorize("stream", "delete", permissionClient), streamHandler.DeleteStream)
-		auth.POST("/api/content/:id/upload", middleware.Authorize("stream", "write", permissionClient), streamHandler.UploadVideo)
-		auth.GET("/api/content/:id/download", middleware.Authorize("stream", "read", permissionClient), streamHandler.DownloadStream)
+		auth.POST("/", middleware.Authorize("stream", "write", permissionClient), streamHandler.CreateStream)
+		auth.GET("/:id", middleware.Authorize("stream", "read", permissionClient), streamHandler.GetStream)
+		auth.PATCH("/:id", middleware.Authorize("stream", "write", permissionClient), streamHandler.UpdateStream)
+		auth.DELETE("/:id", middleware.Authorize("stream", "delete", permissionClient), streamHandler.DeleteStream)
+		auth.POST("/:id/upload", middleware.Authorize("stream", "write", permissionClient), streamHandler.UploadVideo)
+		auth.GET("/:id/download", middleware.Authorize("stream", "read", permissionClient), streamHandler.DownloadStream)
 	}
 	return r, nil
 }
